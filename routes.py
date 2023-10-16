61e1a73fcdeddf187f1007c0ab098c803c924d18
Formation <jeanchri.garcia@gmail.com>
2023-10-16 19:16:56 +0200

    added and sorted all the pictures

--- routes.py
+++ routes.py
@@ -9,3 +9,34 @@ def index():
     # Obtenir la date actuelle
     today = datetime.date.today()
     return render_template("index.html", today=today)
+
+
+@app.route('/men')
+def men():
+    return render_template("men.html", title="Men")
+
+
+@app.route('/women')
+def women():
+    return render_template("women.html", title="Women")
+
+@app.route('/about_us')
+def about_us():
+    return render_template("about_us.html", title="About Us")
+
+@app.route('/lookbook')
+def lookbook():
+    return render_template("lookbook.html", title="Lookbook")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html", title="Contact")
+
+@app.route('/login')
+def login():
+    return render_template("login.html", title="Login")
+
+@app.route('/basket')
+def basket():
+    return render_template("basket.html", title="Panier")
